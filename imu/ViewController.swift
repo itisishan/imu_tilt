df6b4218209b741cd458e24cdae12c6169aeedba
itisishan <ishan.chatterjee1@gmail.com>
2020-05-16 10:32:48 -0700

    Initial Commit

---
+++ imu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imu
+//
+//  Created by Ishan Chatterjee on 5/16/20.
+//  Copyright © 2020 Ishan Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
